cec213c60748856ff3aaa23ef6fc5401375f8958
Justin Miller <jayman1294@gmail.com>
2019-10-13 08:30:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+new_array = []
+
+  if array.length == 1 
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_array = "and " + array.pop
+    array << new_array
+    array.join(", ")
+  end
 end
