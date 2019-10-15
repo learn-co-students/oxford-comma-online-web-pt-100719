7677cb11693f65f0ebd3e2c27a94eb691e9e964b
Derric Fragosa <fderric@gmail.com>
2019-10-15 06:04:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,17 @@
 array = ["this","is","music","fun"]
 
 def oxford_comma(array)
-   string_array = []
-   array.each do  |strings, add_and, oxford, format|
+   if array.size == 1 
+    array.join
+   elsif array.size == 2 
+   end 
    
  
 end
+
+
 oxford_comma("")
-#Oxford comma would be "this, is, and music" basically a comma b4 "and"
+#Oxford comma would be this, is, and music basically a comma b4 and
 #can use .insert(2,"and") puts and @ index 2
 #also use .split(",")
 #Notes 1st change 2 string,2nd add and, 3rd add Oxford comma b4 and, 
