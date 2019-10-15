49616b46610f9c7fdd582194b7394aa1eb6ddc78
Derric Fragosa <fderric@gmail.com>
2019-10-15 04:40:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
-array = ["this","is","a","song"]
+array = ["this","is","music"]
 
 def oxford_comma(array)
  i = 0 
- string_array = [] 
-array.each do |array|
-   puts string_array.join
+ string_array = []
+ 
   end 
 
 
 end
+
+#Oxford comma would be "this, is and music"
+#can use .insert(2,"and") puts and @ index 2
