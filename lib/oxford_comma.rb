a92b1456ea951f5d1589218b5bc3a5e5b91995e5
lynchrFI <lynchrbusiness@gmail.com>
2019-10-16 16:34:37 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
-end
+    formatted_string = ""
+     array.each_with_index { |word, index|
+          if(index==0)
+              formatted_string << word
+          elsif(index>0 && index<(array.length - 1) && array.length>2)
+              formatted_string << ", #{word}"
+          elsif(index > 0 && array.length==2)
+              formatted_string << " and #{word}"
+          else
+              formatted_string << ", and #{word}"
+          end
+        }
+    formatted_string
+    end
