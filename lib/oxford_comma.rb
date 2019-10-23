d1a088ff9e57f8f7ceecaad88fea9d75da39752e
Henry Allan <henryallan50@gmail.com>
2019-10-23 19:45:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,33 @@
 def oxford_comma(array)
+  
+  if array.size <= 1
 
+  p array.join
+  
+  elsif array.size == 2
+
+   p array.join(" and ")
+   
+  else
+    
+    p array
+    
+    meh = array.last
+    
+    p meh
+    
+    array.pop
+    
+    p array
+    
+    string = array.join(", ")
+    
+    p string
+    
+    string2 = string << ", and #{meh}"
+    
+    p string2
+    
+  end
+  
 end
